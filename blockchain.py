dade390031c777837a6eb7b504325ad0a33674ac
dhillii <david.hill@bison.howard.edu>
2019-05-20 14:15:34 -0400

    Blockchain complete ✅

--- blockchain.py
+++ blockchain.py
@@ -18,14 +18,14 @@ class Blockchain:
     
     def __init__(self):
         self.chain = []
-        self.create_block(proof = 1, prev_hash = '0')
+        self.create_block(proof=1, prev_hash='0')
         
         
     def create_block(self, proof, prev_hash):
         block = {'index': len(self.chain + 1), 
                  'timestamp': str(datetime.datetime.now()), 
                  'proof': proof, 
-                 'prev_hash': previous_hash}
+                 'prev_hash': prev_hash}
         self.chain.append(block)
         return block
         
@@ -43,8 +43,30 @@ class Blockchain:
                 new_proof += 1   
         return new_proof
     
+    def hash(self, block):
+        encoded_block = json.dumps(block, sort_keys=True,).encode()
+        return hashlib.sha256(encoded_block).hexdigest()
+    
+    def is_valid(self, chain):
+        previous_block = chain[0]
+        block_index = 1
+        while block_index < len(chain):
+            block = chain[block_index]
+            if block['prev_hash'] != self.hash(previous_block):
+                return False
+            prev_proof = previous_block['proof']
+            proof = block['proof']
+            hash_operation = hashlib.sha256(str(proof**2 - prev_proof**2).encode()).hexdigest();
+            if hash_operation[:4] != '0000':
+                return False
+            previous_block = block
+            block_index += 1
+        return True
     
     
+        
+    
+        
                 
             
         
