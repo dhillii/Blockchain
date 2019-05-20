8970f1ff89f380527f43fca567f77676c14ac5b7
dhillii <david.hill@bison.howard.edu>
2019-05-20 13:30:43 -0400

    added create block function

--- blockchain.py
+++ blockchain.py
@@ -14,6 +14,21 @@ import json
 from flask import Flask
 import jsonify
 
-class 
+class Blockchain:
+    
+    def __init__(self):
+        self.chain = []
+        self.create_block(proof = 1, prev_hash = '0')
+        
+        
+    def create_block(self, proof, prev_hash):
+        block = {'index': len(self.chain + 1), 
+                 'timestamp': str(datetime.datetime.now()), 
+                 'proof': proof, 
+                 'prev_hash': previous_hash}
+        self.chain.append(block)
+        return block
+        
+    
 
 
