a311064b4a8c446b02f62ac06728e279d73ef84d
dhillii <david.hill@bison.howard.edu>
2019-05-20 14:47:02 -0400

    added get chain

--- miner.py
+++ miner.py
@@ -16,6 +16,33 @@ from blockchain.py import Blockchain
 app = Flask(__name__)
 
 #Create Blockchain
-
 blockchain = Blockchain()
+
+# Mine Block
+@app.route('/mine_block', methods=['GET'])
+def mine_block():
+    prev_block = blockchain.get_prev_block()
+    prev_proof = prev_block['proof']
+    proof = blockchain.proof_of_work(prev_proof)
+    prev_hash = blockchain.hash(prev_block)
+    block = blockchain.create_block(proof, prev_hash)
+    response = {'message': 'Congradulations! You mined a block!', 
+                'index': block['index'], 
+                'timestamp': block['timestamp'], 
+                'proof' : block['proof'], 
+                'prev_hash': block['prev_hash']}
+    return jsonify(response), 200
+
+# Get Blockchain
+@app.route('/get_chain', methods=['GET'])
+def get_chain():
+    response = {'chain': blockchain.chain,
+                'chain_length': len(blockchain.chain)}
+    return jsonify(response), 200
+    
+
+
+
+
+
 
